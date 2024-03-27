25bc4499053428f616afbc8b16abf790fa323bd0
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-27 17:23:52 +0100

    Updated GitIgnor

---
+++ Assets/Scripts/AttachmentData/AttachmentData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Attachment data")]
+public class AttachmentData : ScriptableObject 
+{
+    
+}
+
