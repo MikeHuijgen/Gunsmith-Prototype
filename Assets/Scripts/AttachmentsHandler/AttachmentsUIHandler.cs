2681538c82df983f1f336bee373ec0a3286f57e3
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-30 16:20:51 +0100

    Attachment slot activation
    The system now look on how many attachments are in a category. When is has the length of the list it will activate that amount of attachment slots

--- Assets/Scripts/AttachmentsHandler/AttachmentsUIHandler.cs
+++ Assets/Scripts/AttachmentsHandler/AttachmentsUIHandler.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UI;
 
 public class AttachmentsUIHandler : MonoBehaviour
 {
     [SerializeField] private GunsmithSystem gunsmithSystem;
+    [SerializeField] private GameObject attachmentSlot;
+    [SerializeField] private GameObject attachmentHolder;
+    
     private string _selectedCategory;
     private AttachmentCategory _attachmentCategory;
+    private List<GameObject> _attachmentSlotsPool;
+    private int poolSize = 20;
+
+    private void Awake()
+    {
+        FillPool();
+    }
 
     public void SetSelectedAttachmentCategory(string category)
     {
         _selectedCategory = category;
         _attachmentCategory = gunsmithSystem.GetAttachmentsFromCategory(_selectedCategory);
-        print(_attachmentCategory.name);
+        SetUpAttachmentSlots();
+    }
+
+    private void FillPool()
+    {
+        _attachmentSlotsPool = new List<GameObject>();
+        for (var i = 0; i < poolSize; i++)
+        {
+            var newAttachmentSlot = Instantiate(attachmentSlot, attachmentHolder.transform);
+            newAttachmentSlot.SetActive(false);
+            _attachmentSlotsPool.Add(newAttachmentSlot);
+        }
+    }
+
+    public void ResetPool()
+    {
+        foreach (var attachmentSLot in _attachmentSlotsPool)
+        {
+            attachmentSLot.SetActive(false);
+        }
+    }
+
+    private void SetUpAttachmentSlots()
+    {
+        var attachmentSlotAmount = _attachmentCategory.attachments.Length;
+
+        for (var i = 0; i < attachmentSlotAmount; i++)
+        {
+            if (_attachmentSlotsPool[i].activeInHierarchy) continue;
+            _attachmentSlotsPool[i].SetActive(true);
+        }
     }
     
 }
